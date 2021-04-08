281f15fbc58e17716e35219824d003d009405763
Bhamini <bhaminishivram@gmail.com>
2021-04-08 10:39:00 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React,{Component} from "react";
 import "./App.css"; 
 
-class App extends Component {
+class App extends Component { 
+    constructor(props){
+      super(props); 
+
+      this.state ={
+        firstName :null, 
+        lastName:null, 
+        email:null, 
+        password:null, 
+        formErrors:{
+          
+        }
+      }
+    }
   render(){
 
  return (
